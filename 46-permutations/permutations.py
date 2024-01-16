3668749d1f57cb886f8a5d4e918747fa78613bed
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-01-17 00:12:19 +0530

    Time: 39 ms (84.72%) | Memory: 17.5 MB (32.22%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,5 +1,3 @@
-from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        l = list(permutations(nums))
-        return l
+        return list(itertools.permutations(nums))
