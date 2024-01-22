08990bde651fbd2f43194d2f781841adb180dc18
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-01-22 16:55:39 +0530

    Time: 137 ms (99.66%) | Memory: 18.2 MB (77.07%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,19 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-
-        i = 1
-        while 1:
-            if i not in nums:
-                c = i
-                break
-            i += 1
+        n = len(nums)
         d = {}
         for i in nums:
             if i not in d:
                 d[i] = 1
             else:
-                d[i] += 1
-            if d[i] == 2:
                 b = i
-                break
+        nums.remove(b)
+        c = (n*(n+1)//2)-sum(nums)
         return b,c
