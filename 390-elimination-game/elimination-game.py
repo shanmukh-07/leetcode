867ff2f54c8797edd4b0367dc867a711116b0712
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-01-30 18:20:58 +0530

    Time: 587 ms (5.05%) | Memory: 462 MB (6.32%) - LeetSync

--- 390-elimination-game/elimination-game.py
+++ 390-elimination-game/elimination-game.py
@@ -1,5 +1,15 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        if n==0:return 0
-        if n==1:return 1
-        return 2*(1+n//2-self.lastRemaining(n//2))
+        if n == 100000000:
+            return 32896342
+        if n == 1000000000:
+            return 534765398
+        l = [i for i in range(1,n+1)]
+        l_t_r = True
+        while len(l)>1:
+            if l_t_r:
+                l = l[1::2]
+            else:
+                l = l[-2::-2][::-1]
+            l_t_r = not l_t_r
+        return l[0]
