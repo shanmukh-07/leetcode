0811165bd2796aaf02a7c0b5dab15e8aeeb53277
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-01-30 17:44:56 +0530

    Time: 210 ms (35.11%) | Memory: 18.1 MB (63.31%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,14 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def fun(arr,l,h,t):
-            if l>h:return -1
-            m = (l+h)//2
-            if arr[m] == t:
-                return m
-            elif t>arr[m]:
-                return fun(arr,m+1,h,t)
-            else:
-                return fun(arr,l,m-1,t)
         l = 0
         r = len(nums)-1
-        return fun(nums,l,r,target)
+        while l<=r:
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            elif nums[m]<target:
+                l = m+1
+            else:
+                r = m-1
+        return -1
+
+
+        # def fun(arr,l,h,t):
+        #     if l>h:
+        #         return -1
+        #     m = (l+h)//2
+        #     if arr[m] == t:
+        #         return m
+        #     elif t>arr[m]:
+        #         return fun(arr,m+1,h,t)
+        #     else:
+        #         return fun(arr,l,m-1,t)
+        # l = 0
+        # r = len(nums)-1
+        # return fun(nums,l,r,target)
