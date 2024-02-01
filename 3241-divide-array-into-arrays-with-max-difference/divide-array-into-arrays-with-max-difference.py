3e085bc0bdd59f5a36e0671da85a70252b037d78
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-01 11:19:33 +0530

    Time: 688 ms (98.57%) | Memory: 30.7 MB (96.29%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(nums)
         nums.sort()
         l = []
-        c = True
+        c = 0
         for i in range(0,n,3):
             a = nums[i:i+3]
             l.append(a)
-            if abs(a[2]-a[0]) > k:
-                c = False
-        if c:
+            if abs(a[2]-a[0]) <= k:
+                c += 1
+        if len(l) == c:
             return l
         return []
