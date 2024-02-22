1de7621d24a86edda58ac38629f89eb331a4fe38
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-22 10:10:40 +0530

    Time: 595 ms (81.84%) | Memory: 21.5 MB (86.28%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -2,19 +2,19 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         d = {i+1:[] for i in range(n)}
         for i in trust:
-            d[i[0]].append(i[1])   
+            d[i[0]].append(i[1])
         c = -1
-        l = []
+        ll = []
         for i,j in d.items():
             if len(j) == 0:
                 c = i
             else:
-                l.append((i,j))
-        dd = 0
+                ll.append((i,j))
+        tc = 0
         if c != -1:
-            for i,j in l:
+            for i,j in ll:
                 if c in j:
-                    dd += 1
-        if dd != n-1:
+                    tc += 1
+        if tc != n-1:
             c = -1
         return c
