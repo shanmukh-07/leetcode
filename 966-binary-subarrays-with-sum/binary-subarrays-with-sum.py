beb4b4a5c2bbe6eedc9c3725ec0abf448aa22bc6
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-03-14 16:59:45 +0530

    Time: 210 ms (91.15%) | Memory: 17.4 MB (82.06%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,10 +1,26 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        d = collections.Counter({0:1})
-        c = 0
-        ps = 0
-        for i in nums:
-            ps += i
-            c += d[ps-goal]
-            d[ps] += 1
-        return c
+        c,res,d = 0,0,0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                goal -= 1
+                c = 0
+            while goal == 0 and d <= i:
+                goal += nums[d]
+                d += 1
+                c += 1
+                if d > i-goal+1:
+                    break
+            while goal < 0 :
+                goal += nums[d]
+                d += 1
+            res += c
+        return res
+        # d = collections.Counter({0:1})
+        # c = 0
+        # ps = 0
+        # for i in nums:
+        #     ps += i
+        #     c += d[ps-goal]
+        #     d[ps] += 1
+        # return c
